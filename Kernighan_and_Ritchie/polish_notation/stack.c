51fe7b529ba5d9bdfb6ee38732f220c23c1f4433
theSaintBelial <xfearlessrizzze@gmail.com>
2019-12-31 02:12:38 +0700

    Nothing is ok

--- Kernighan_and_Ritchie/polish_notation/stack.c
+++ Kernighan_and_Ritchie/polish_notation/stack.c
@@ -11,7 +11,7 @@ void push(double f) // положить новое значение в стек
     if (sp < MAXVAL)
         val[sp++] = f;
     else
-        printf("error: stack is full, %d cannot be placed\n", f);
+        printf("error: stack is full, %g cannot be placed\n", f);
 }
 
 double pop(void) // взять с вершины стека и выдать в качестве результата
